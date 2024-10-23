9fea8815d8add691dbaa68257c0a2d98fa799c29
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 01:45:13 -0500

    create exercises-14
    Basic exercise using while loop.

---
+++ python-exercises/exercises-14.py
@@ -0,0 +1,21 @@
+print("A N I M A L S 🤠 F A R M")
+print()
+
+exit = None
+
+while exit != "yes":
+    animal = input("What animal do you want? : ")
+
+    if animal == "cow":
+        print("A 🐮 cow goes moo")
+        print()
+    elif animal == "dog":
+        print("A 🐶 dog goes Wof Wof")
+        print()
+    elif animal == "cat":
+        print("A 😺 cat goes Miau Miau")
+        print()
+    else:
+        print("I dont this animal, sorry!")
+
+    exit = input("Do you want to exit? > ")
