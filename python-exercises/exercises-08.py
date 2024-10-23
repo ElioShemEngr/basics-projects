e23c484f712a527563ed9cb9134eda9a50096c20
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 01:18:09 -0500

    create exercises-08
    Exercise using logical and comparison operators in conditionals.

---
+++ python-exercises/exercises-08.py
@@ -0,0 +1,16 @@
+print ("wholesome Positivity Machine")
+print ()
+
+nameUser = input ("Who are you?: ")
+print ()
+
+response = input ("What do you want to achieve today?: ")
+print ()
+
+scale = input ("On Scale of 1 - 10 how do you feel today? (1: 😢,10:🥳): ")
+if scale == "1" or scale == "2" or scale == "3":
+  print ("Hey ", nameUser, "keep your chin up! Today you're going to ", response, "in the most amazing way, simply by being you - YOU ROCK!")
+elif scale == "4" or scale == "5" or scale == "6":
+  print ("Hey ", nameUser, "You're going on!")
+else:
+  print ("Hey ", nameUser, "Perfect, thank you for that 🥳")
