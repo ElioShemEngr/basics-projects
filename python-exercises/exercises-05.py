0f79733217b6e36088c69af866a7ea3b1ca24a9c
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 01:14:20 -0500

    create exercises-05
    Basic exercise using conditionals.

---
+++ python-exercises/exercises-05.py
@@ -0,0 +1,29 @@
+print("Marvel Movie Character Creator")
+print("--")
+
+response_1 = input("Your super power is the spider web?: ")
+
+if response_1 == "yes":
+    print("You are Spider Man")
+else:
+    print("Then you're not Spider Man")
+    print("Ok, next questions...")
+print("")
+
+response_2 = input("You are a big man green?: ")
+
+if response_2 == "yes":
+    print("you are Hulk")
+else:
+    print("Then you're not Hulk")
+    print("Ok, next questions...")
+print("")
+
+response_3 = input("You are an engineer?: ")
+
+if response_3 == "yes":
+    print("you are Iron Man")
+else:
+    print("Then you're Iron Man")
+    print("Ok, I give up on you...")
+print("")
