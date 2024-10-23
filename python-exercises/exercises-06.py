ad9db058bd50e940b6cd4d276ef408ed5fa8e1aa
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 01:15:47 -0500

    create exercises-06
    Basic authentication system exercise.

---
+++ python-exercises/exercises-06.py
@@ -0,0 +1,11 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "Elio" and password == "Xai9kzcmPettelw2":
+  print("Have a great day Elio!")
+elif  username == "Elio" or password == "Xai9kzcmPettelw2":
+  print("Hello there Elio, what a lovely accent you have, you could have charmed your way in here even without a password!")
+else:
+  print("Don't forget to wear a hat in the sun!")
