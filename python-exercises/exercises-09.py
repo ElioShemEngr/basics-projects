610c7541b9c4de16ad8ce72b5ace680da0ddc5e0
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 01:19:42 -0500

    create exercises-09
    Exercise using logical and comparison operators in conditionals.

---
+++ python-exercises/exercises-09.py
@@ -0,0 +1,24 @@
+print("Generation Identifier")
+print("---------------------")
+print()
+
+nameUser = input("What's your name?: ")
+yearBirth = int(input("Which year were you born?: "))
+if yearBirth >=1883 and yearBirth <=1900:
+  print("Hah!", nameUser, ", You are the Lost Generation")
+elif yearBirth >=1901 and yearBirth <=1927:
+  print("Hah!", nameUser, ", You are the Greatest Generation")
+elif yearBirth >=1928 and yearBirth <=1945:
+  print("Hah!", nameUser, ", You are Silent Generation")
+elif yearBirth >=1946 and yearBirth <=1964:
+  print("Hah!", nameUser, ", You are Baby Boomers")
+elif yearBirth >=1965 and yearBirth <=1980:
+  print("Hah!", nameUser, ", You are the Generation X")
+elif yearBirth >=1981 and yearBirth <=1996:
+  print("Hah!", nameUser, ", You are the Millenials")
+elif yearBirth >=1997 and yearBirth <=2012:
+  print("Hah!", nameUser, ", You are Generation Z")
+elif yearBirth >=2013 and yearBirth <=2023:
+  print("Hah!", nameUser, ", You are Generation Alpha")
+else:
+  print("In outer year please")
