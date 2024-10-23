802473df6269c41853e4923a286741bdfe3de47e
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 18:15:30 -0500

    create exercises-17
    Game exercise to guess the number.

---
+++ python-exercises/exercises-17.py
@@ -0,0 +1,22 @@
+print("One-Million-to-One")
+print()
+
+round = 1
+
+while True:
+    numberGuess = int(input("What is your guess?: "))
+
+    if numberGuess == 50:
+        print("Correct")
+        break
+    elif numberGuess < 50:
+        print("Too low")
+        round += 1
+    elif numberGuess > 50:
+        print("Too high")
+        round += 1
+    else:
+        print("Input a correct value")
+        continue
+
+print("It took you ", round, "guesses to get it correct!")
