33ff8775e3f8d6394890cde550ce38111f1f58a9
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-24 12:47:36 -0500

    create exercises-70

---
+++ python-exercises/exercises-70.py
@@ -0,0 +1,19 @@
+import plotly.express as px
+
+country = input("Enter the country name: ")
+
+data = {
+    'country' : [country], 
+    'values' : [100]
+}
+
+fig = px.choropleth(
+    data,
+    locations='country',
+    locationmode='country names',
+    color='values',
+    color_continuous_scale='Inferno',
+    title=f'Country Map Highlighting {country}'
+)
+
+fig.show()
