e77aed91290b02c0b50788228c7af06ed2d761e8
Elio Shem B. <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-23 19:20:58 -0500

    refactor: user data with for loop
    Add user data input via for loop.

--- python-exercises/exercises-40.py
+++ python-exercises/exercises-40.py
@@ -3,18 +3,10 @@ print()
 
 webPage = {"name" : None, "url" : None, "desc" : None, "rating" : None}
 
-nameWeb = input("name : ")
-urlWeb = input("url : ")
-descWeb = input("desc : ")
-ratingWeb = input("rating : ")
-
-webPage["name"] = nameWeb
-webPage["url"] = urlWeb
-webPage["desc"] = descWeb
-webPage["rating"] = ratingWeb
+for key, value in webPage.items():
+    webPage[key] = input(f"{key} : ")
 
 print()
 
 for key, value in webPage.items():
     print(f"{key} : {value}")
-
