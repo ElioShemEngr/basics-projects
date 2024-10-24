c9c1b9f665a06b4255fd91aec0e7c04dab36f08c
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-24 13:08:12 -0500

    basic exercises dart

---
+++ dart-exercises/exercises-02.dart
@@ -0,0 +1,24 @@
+// Exercise 02
+
+void main() {
+  print('Getting to know you!');
+  print('');
+
+  // Variables
+  String my_Name = 'Elio';
+  String my_Food = 'peruvian food';
+  String my_Music = 'Rock and Roll';
+  String my_Live = 'spain';
+
+  // Display results
+  print('');
+  print('You are...');
+  print(my_Name);
+  print('You are probably hungry for...');
+  print(my_Food);
+  print('And you are definitely getting your groove on to...');
+  print(my_Music);
+  print('Living in the amazing');
+  print(my_Live);
+
+}
---
+++ dart-exercises/exercises-03.dart
@@ -0,0 +1,20 @@
+// Exercise 03
+//Write a function that takes two numbers as arguments and returns their sum. 
+//Call the function with different pairs of numbers and display the result.
+
+void main() {
+  print('Sum of two numbers');
+  print('');
+
+  // Variables
+  var first_num = 10.15;
+  var second_num = 20;
+
+  // Addition operation
+  var sum_num = first_num + second_num;
+
+  // Display results
+  print('The first number is : $first_num');
+  print('The second number is : $second_num');
+  print('The result of sum is : $sum_num');
+}
