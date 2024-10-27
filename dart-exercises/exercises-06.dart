e856b6e6f50b5c4feffa366bb6e798aee02bed02
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-10-27 17:17:46 -0500

    create exercises-06

---
+++ dart-exercises/exercises-06.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+main(){
+  String username;
+  String password;
+
+  print("MY LOGIN SYSTEM");
+  print("");
+
+  stdout.writeln("Username :");
+  username = stdin.readLineSync()!;
+
+  stdout.writeln("Password :");
+  password = stdin.readLineSync()!;
+
+  if (username == 'Elio' && password == 'Xai9kzcm'){
+    print("Have a great day Elio");
+  } else if (username == 'Elio' || password == 'Xai9kzcm'){
+    print("User o Password incorrect");
+  } else {
+    print("this user not exist!");
+  }
+
+}
