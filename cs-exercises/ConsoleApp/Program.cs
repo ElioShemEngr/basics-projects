c89894bbfb4ab26d7c3c47d0ecef01d458e2be6f
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-11-13 22:42:01 -0500

    basics programs Csharp

---
+++ cs-exercises/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Mi primera App de Consola");
+            int edad = Console.Read();
+            Console.WriteLine($"Mi edad es de : {edad}");
+        
+        }
+    }
+}
