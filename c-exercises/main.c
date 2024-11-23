0048129dd1622cd919fd724d8ee2bb67e49047b1
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-11-23 00:37:03 -0500

    add exe

---
+++ c-exercises/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+  
+    return 0;
+}
+
+//Ejecutar en una terminal integrada con la siguiente declaracion
+// gcc main.c -o main -> (para crear un ejecutable)
+//luego ejecutamos
+//./main.exe 
