a50c0e54992e09f5b5d0721f2b251a98c13443f5
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2024-11-23 00:38:12 -0500

    add exe

---
+++ cpp-exercises/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+//Ejecutar en una terminal integrada con la siguiente declaracion
+// g++ hello.cpp -o hello -> (para crear un ejecutable)
+//luego ejecutamos
+//./hello.exe 
