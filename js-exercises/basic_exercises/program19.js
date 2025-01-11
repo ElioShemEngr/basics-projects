8c6aaffe813d1d2a39675e7b789a4c2bf5cd9a96
ElioShemEngr <122136358+ElioShemEngr@users.noreply.github.com>
2025-01-10 20:40:40 -0500

    update exercises 19 and 20

--- js-exercises/basic_exercises/program19.js
+++ js-exercises/basic_exercises/program19.js
@@ -1,1 +1,16 @@
 // 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
+
+const nameValid = (name = '') => {
+    if (!name) return console.warn("No ingresaste una cadena de texto");
+
+    if (typeof name !== "string")
+      return console.error("El valor ingresado no es una cadena de texto");
+
+    let expReg = /^[A-Za-zñÑ\s]+$/g.test(name);
+
+    return (expReg)
+    ? console.info(`"${name}", es un nombre valido`)
+    : console.info(`"${name}", NO es un nombre valido`);
+};
+
+nameValid("Elio Solis Ñaupari");
--- js-exercises/basic_exercises/program20.js
+++ js-exercises/basic_exercises/program20.js
@@ -1,1 +1,20 @@
 // 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
+
+const emailValid = (email = '') => {
+    if (!email) return console.warn("No ingresaste una cadena de texto");
+
+    if (typeof email !== "string")
+      return console.error("El valor ingresado no es una cadena de texto");
+
+    let expReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(email);
+
+    // /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email);
+
+    return (expReg)
+    ? console.info(`"${email}", es un email valido`)
+    : console.info(`"${email}", NO es un email valido`);
+};
+
+emailValid("ejem**/pló.ema_il@gmail.com");
+
+
