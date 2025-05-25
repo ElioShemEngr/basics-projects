865611ae6d56b49adefa35ef17e937d46f8d72ee
elioshemDev <122136358+ElioShemEngr@users.noreply.github.com>
2025-05-24 20:34:10 -0500

    update excercise 06

--- python-exercises/exercises-06.py
+++ python-exercises/exercises-06.py
@@ -5,7 +5,5 @@ password: str = input("Password > ")
 
 if username == "Elio" and password == "Xai9kzcmPettelw2":
   print("Have a great day Elio!")
-elif  username == "Elio" or password == "Xai9kzcmPettelw2":
-  print("Hello there Elio, what a lovely accent you have, you could have charmed your way in here even without a password!")
 else:
   print("Don't forget to wear a hat in the sun!")
