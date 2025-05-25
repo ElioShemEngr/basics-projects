f4c01c9ab79874418876d2b166c216e0b6c7bd27
elioshemDev <122136358+ElioShemEngr@users.noreply.github.com>
2025-05-24 20:30:52 -0500

    create intro language c#

---
+++ cs-exercises/IntroCSharp/IntroCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntroCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Comentarios en una linea
+
+            /*
+             * Comentarios en multiples
+             * lineas
+            */
+
+            Console.WriteLine("Hola Mundo");        
+        }
+    
+    
+    
+    }
+
+
+}
