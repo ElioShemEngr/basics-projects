7be28c363c20546471fa4ea7949e6dbb90451537
elioshemdev <122136358+ElioShemEngr@users.noreply.github.com>
2025-11-18 02:07:52 -0500

    holamundo rust

--- rust-exercises/holamundo/target/debug/deps/holamundo.d
+++ rust-exercises/holamundo/target/debug/deps/holamundo.d
@@ -1,5 +1,5 @@
-D:\Projects-git\basics-projects\rust-exercises\holamundo\target\debug\deps\holamundo.exe: src\main.rs
-
 D:\Projects-git\basics-projects\rust-exercises\holamundo\target\debug\deps\holamundo.d: src\main.rs
+
+D:\Projects-git\basics-projects\rust-exercises\holamundo\target\debug\deps\holamundo.exe: src\main.rs
 
 src\main.rs:
