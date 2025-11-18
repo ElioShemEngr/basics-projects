e8af8b615b43141d872a7514e2f98c50e9d93671
elioshemdev <122136358+ElioShemEngr@users.noreply.github.com>
2025-11-18 02:06:30 -0500

    holamundo

--- c-exercises/00_hola_mundo/main.c
+++ c-exercises/00_hola_mundo/main.c
@@ -3,15 +3,22 @@
 int main()
 {
     // 0. Hola Mundo!
-    printf("I like pizza!\n");
-    printf("It's really good!\n");
+    printf("Hola Mundo desde C!\n");
+    printf("Mi nombre es ElioshemDev!\n");
 
     // 1. Variables
+    // Integer
     int age = 30;
     int year = 2025;
+    // float
+    float tail = 1.78;
+    // char
+    char letra = 'A';
 
-    printf("You are %d years old\n", age);
-    printf("The year is %d", year);
+    printf("Tienes %d anos de edad \n", age);
+    printf("Tu altura es de %.2f \n", tail);
+    printf("El ano actual es %d \n", year);
+    printf("Esta es la letra : %c", letra);
 
     return 0;
 }
